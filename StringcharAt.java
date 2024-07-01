fc4cce730c51cc8e73d7e27a24b80665791f6cfe
ManchukondaVaishnavi <142039183+ManchukondaVaishnavi@users.noreply.github.com>
2024-07-01 09:54:07 +0530

    StringcharAt

---
+++ StringcharAt.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class StringcharAt{
+public static void main(String[] args){
+Scanner s=new Scanner(System.in);
+String str=s.next();
+for(int i=0;i<str.length();i++){
+System.out.print(str.charAt(i));
+}
+}
+}
