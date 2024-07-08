f1809a2edddc725eec983625025d36897a1619f7
ManchukondaVaishnavi <142039183+ManchukondaVaishnavi@users.noreply.github.com>
2024-07-08 13:49:20 +0530

    Print sub arrays

---
+++ PrintSubArray.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class PrintSubArray{
+public static void main(String[] args){
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int a[]=new int[n];
+for(int i=0;i<n;i++){
+a[i]=s.nextInt();
+}
+for(int i=0;i<n;i++){
+for(int j=i;j<n;j++){
+for(int k=i;k<=j;k++){
+System.out.print(a[k]+" ");
+}
+System.out.println();
+}
+}
+}
+}
