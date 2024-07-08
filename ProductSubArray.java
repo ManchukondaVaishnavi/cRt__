d4ecbdaff81ff1853d610a5d9876fd22096b8f48
ManchukondaVaishnavi <142039183+ManchukondaVaishnavi@users.noreply.github.com>
2024-07-08 14:02:48 +0530

    Product sub array

---
+++ ProductSubArray.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class ProductSubArray{
+public static void main(String[] args){
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int a[]=new int[n];
+for(int i=0;i<n;i++){
+a[i]=s.nextInt();
+}
+int tar=s.nextInt();
+for(int i=0;i<n;i++){
+int mul=1;
+for(int j=i;j<n;j++){
+mul=mul*a[j];
+if(tar==mul){
+System.out.print("YES");
+return;
+}
+}
+}
+System.out.print("No");
+}
+}
