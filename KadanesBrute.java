ed60a98a5e018e832c5a133ed71c92e691a1f9c1
ManchukondaVaishnavi <142039183+ManchukondaVaishnavi@users.noreply.github.com>
2024-07-09 14:44:29 +0530

    KadanesBrute

---
+++ KadanesBrute.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class KadanesBrute{
+public static void main(String[] args){
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int a[]=new int[n];
+for(int i=0;i<n;i++){
+a[i]=s.nextInt();
+}
+int sum=0,max=Integer.MIN_VALUE;
+for(int i=0;i<n-1;i++){
+
+for(int j=i;j<n;j++){
+sum=0;
+for(int k=i;k<=j;k++){
+sum+=a[k];
+if(max<sum){
+max=sum;
+}
+}
+}
+}
+System.out.print(max);
+}
+}
