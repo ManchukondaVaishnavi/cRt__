697ddd472586498089db01fe633e7da433511998
ManchukondaVaishnavi <142039183+ManchukondaVaishnavi@users.noreply.github.com>
2024-07-09 14:45:02 +0530

    Kadanes order of n

--- Kadanes.java
+++ Kadanes.java
@@ -7,19 +7,16 @@ int a[]=new int[n];
 for(int i=0;i<n;i++){
 a[i]=s.nextInt();
 }
-int sum=0,max=Integer.MIN_VALUE;
-for(int i=0;i<n-1;i++){
-
-for(int j=i;j<n;j++){
-sum=0;
-for(int k=i;k<=j;k++){
-sum+=a[k];
-if(max<sum){
-max=sum;
+int gs=0,cs=0;
+for(int i=0;i<n;i++){
+cs+=a[i];
+if(gs<cs){
+gs=cs;
+}
+if(cs<0){
+cs=0;
 }
 }
-}
-}
-System.out.print(max);
+System.out.print(gs);
 }
 }
